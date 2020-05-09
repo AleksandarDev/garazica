1b08ce7f35ae6a8a8a8357aa2924bf3e474d4c55
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2020-05-09 17:15:38 +0200

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="hr">
         <Head>
-          <meta charset="utf-8" />
+          <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="viewport"
